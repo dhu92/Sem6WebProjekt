ba225f08e2b0352223ad2f318a06feedd327547c
cbl6499 <clemens.blecha@students.fhv.at>
2018-03-21 08:58:07 +0100

    doctrine + ticket.php

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ClemensB
+ * Date: 21.03.18
+ * Time: 08:52
+ */
+
+namespace App\Entity;
+
+/**
+ * @ORM/Entity
+ */
+
+class Ticket
+{
+    /**
+     * @ORM/Column(type="integer")
+     * @ORM/GeneratedValue
+     * @ORM/id
+     */
+    private $id;
+    /**
+     * @ORM/Column(type="text")
+     */
+    private $message;
+
+}
