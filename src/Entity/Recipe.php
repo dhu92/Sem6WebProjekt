6e6b78d5abd8534ddcfb921f7d67fc67b769d195
dhu92 <dhutter92@gmail.com>
2018-03-28 08:26:15 +0200

    RecipeTranslation

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -21,6 +21,9 @@ class Recipe
      */
     private $id;
 
-
+    /**
+     * @OneToMany(targetEntity="RecipeTranslation")
+     * @JoinColumn(name="id", referencedColumnName="recipeID")
+     */
     private $translation;
 }
--- src/Entity/RecipeTranslation.php
+++ src/Entity/RecipeTranslation.php
@@ -21,6 +21,9 @@ class RecipeTranslation {
      * @ORM/id
      */
     private $id;
+
+
+    private $recipeID;
     private $name;
     private $description;
     private $preperation;
