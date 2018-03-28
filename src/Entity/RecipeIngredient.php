528eb6a4e47426d38b70478812c74e15471379c7
cbl6499 <clemens.blecha@students.fhv.at>
2018-03-28 08:20:52 +0200

    recipeIngredient

---
+++ src/Entity/RecipeIngredient.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ClemensB
+ * Date: 28.03.18
+ * Time: 08:14
+ */
+
+namespace App\Entity;
+
+/**
+ * @ORM/Table(name="RecipeIngredient")
+ * @ORM/Entity
+ */
+
+class RecipeIngredient
+{
+    /**
+     * @ORM/Column(type="integer")
+     * @ORM/GeneratedValue
+     * @ORM/id
+     */
+    private $id;
+
+    /**
+     * @OneToOne(targetEntity="Recipe")
+     * @JoinColumn(name="recipeID", referencedColumnName="id")
+     */
+    private $recipeID;
+
+    /**
+     * @OneToOne(targetEntity="Ingredient")
+     * @JoinColumn(name="ingredientID", referencedColumnName="id")
+     */
+    private $ingredientID;
+
+    /**
+     * @ORM/Column(type="integer")
+     * @ORM/amount
+     */
+    private $amount;
+
+    /**
+     * @ORM/Column(type="string")
+     * @ORM/measurement
+     */
+    private $measurement;
+
+}
