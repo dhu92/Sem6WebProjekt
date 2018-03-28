438722520c8bf66be9802b303ea2241e6b8b6f3b
dhu92 <dhutter92@gmail.com>
2018-03-28 10:55:51 +0200

    gg ez exception

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Language;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,8 +14,29 @@ class LanguageController extends Controller
      */
     public function index()
     {
-        return $this->render('language/index.html.twig', [
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $language = new Language();
+        $language->setName('German');
+        $entityManager->persist($language);
+        $entityManager->flush();
+       /* return $this->render('language/index.html.twig', [
             'controller_name' => 'LanguageController',
-        ]);
+        ]);*/
+       return new Response('Saved german language');
+    }
+
+    /**
+     * @Route("/getlanguage", name="language")
+     */
+    public function getLanguages(){
+        $language = $this->getDoctrine()->getRepository(Language::class)->find(1);
+        if($language){
+            throw $this->createNotFoundException(
+                'No language found'
+            );
+        }
+        return new Response('Check out this great language: '.$language->getName());
+
     }
 }
