85130a7061dcc6b8645da6bc480f94e14abe6c5d
cbl6499 <clemens.blecha@students.fhv.at>
2018-05-30 16:13:42 +0200

    monolog added

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-
     App\SymfoniacNewsletterBundle\SymfoniacNewsletterBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
