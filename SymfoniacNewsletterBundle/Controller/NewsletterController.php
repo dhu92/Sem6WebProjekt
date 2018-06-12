8518d75249064d7fddbfb646ec1c524ce8ae07f8
ChristophRheinberger <crh6731@students.fhv.at>
2018-06-12 15:58:08 +0200

    vong de minutes her

--- SymfoniacNewsletterBundle/Controller/NewsletterController.php
+++ SymfoniacNewsletterBundle/Controller/NewsletterController.php
@@ -217,7 +217,7 @@ class NewsletterController extends Controller
                 ."\r\n".$ingredientText
                 ."\r\n".$this->recipeTranslation->getDescription()
                 ."\r\n".$this->recipeTranslation->getPreperation()
-                ."\r\n".$this->recipeTranslation->getDuration()." minutes"
+                ."\r\n".$this->recipeTranslation->getDuration()." min"
                 ."\r\n--------------------------------\r\n"
                 ."\r\n".$recipeText;
 
